11f261831912c32960845026329aa8216c7caeea
vessaldaneshvar <vessaldaneshvar@gmail.com>
2021-01-27 12:52:00 +0330

    delete secret key from settings and add to .env file

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't-u@oct5*9a7i5lbx^)#y&gen#yu44g-aw5&b+p)=9h-pf*b9$'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG")
